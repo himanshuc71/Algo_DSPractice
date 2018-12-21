e8095e9102c2780a75e7db81a34e0a46101f4d14
Himanshu <36586647+himanshuc71@users.noreply.github.com>
2018-12-21 00:25:51 -0800

    Add files via upload

---
+++ Solution.java
@@ -0,0 +1,19 @@
+public class Solution {
+    public static int uniquePaths(int m, int n) {
+        int [][] dp = new int[m][n];  // m columns n rows
+        for (int col = 0; col < m; col++){
+            for (int row = 0; row < n; row++){
+                if (row == 0 || col == 0)
+                    dp[col][row] = 1;
+                else
+                    dp[col][row] = dp [col][row-1] + dp[col-1][row];
+            }
+        }
+        return dp[m][n];   // returns the unique bottomrightCorner
+    }
+
+    public static void main(String[] args) {
+        int result = uniquePaths (3,3);
+        System.out.println(result);
+    }
+}
